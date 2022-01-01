eba42c604716121abe3dd8e263c776c3e328d512
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-01 21:26:25 +0000

    conditional counter colour changes in own function

--- script.js
+++ script.js
@@ -5,17 +5,32 @@ const count = document.querySelector(".count");
 const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (e) => {
+  //if target clicked on within the buttons div has certain elements then perform certain actions based off those elements
   //add event listener to parent element
   if (e.target.classList.contains("add")) {
     count.innerHTML++;
+    setColour();
   }
   if (e.target.classList.contains("subtract")) {
     count.innerHTML--;
+    setColour();
   }
   if (e.target.classList.contains("reset")) {
     count.innerHTML = 0;
+    setColour();
   }
 });
+
+function setColour() {
+  //export colour setting into function
+  if (count.innerHTML > 0) {
+    count.style.color = "green";
+  } else if (count.innerHTML < 0) {
+    count.style.color = "orangered";
+  } else {
+    count.style.color = "white";
+  }
+}
 
 // add.addEventListener("click", () => {
 //   count.innerHTML++;
