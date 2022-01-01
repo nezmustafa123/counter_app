d1560d0c55aaf2b146115eb231aebb0e461299d4
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-01 20:37:23 +0000

    adding event listender to parent element to use event delegation

---
+++ script.js
@@ -0,0 +1,28 @@
+// const add = document.querySelector(".add");
+// const resetCount = document.querySelector(".reset");
+// const sub = document.querySelector(".subtract");
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+  //add event listener to parent element
+  if (e.target.classList.contains("add")) {
+    count.innerHTML++;
+  }
+  if (e.target.classList.contains("subtract")) {
+    count.innerHTML--;
+  }
+  if (e.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+  }
+});
+
+// add.addEventListener("click", () => {
+//   count.innerHTML++;
+// });
+// sub.addEventListener("click", () => {
+//   count.innerHTML--;
+// });
+// resetCount.addEventListener("click", () => {
+//   count.innerHTML = 0;
+// });
